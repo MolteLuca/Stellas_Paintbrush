f65d7b71f1748bcaa28258db754598552cc4c313
molte <molteluca@gmail.com>
2020-08-23 18:06:31 -0400

    Fixes

--- Stellas_Paintbrush/settings.py
+++ Stellas_Paintbrush/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "kii(f)myg61q+guzvoxxhof&es)kaaxh$dzwecg!jplr=0p6rp"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
